326bd23d2b771496f7515a62291b42b2a6aa951a
mriekkinen <matti.riekkinen@icloud.com>
2023-12-05 14:26:16 +0200

    Minor edits to SignalClient

--- peer-client/src/signalClient.js
+++ peer-client/src/signalClient.js
@@ -1,6 +1,14 @@
+/**
+ * Maintains a WebSocket connection to the signaling server
+ * 
+ * At the moment doesn't do much, just a template to be filled out with
+ * the actual details
+ */
 class SignalClient {
   constructor() {
     this.socket = null
+    this.userId = null
+    this.peers = []
   }
 
   connect = () => {
@@ -10,31 +18,73 @@ class SignalClient {
       "protocolOne",
     )
 
-    this.socket.onerror = console.log
+    this.socket.onerror = console.error
 
-    this.socket.onopen = () => {
-      console.log("[signalClient] Connection established...")
-    }
+    // On connection established...
+    this.socket.onopen = this.onOpen
 
     // On receipt of a message...
-    this.socket.onmessage = event => {
-      const msg = JSON.parse(event.data)
-      switch (msg.type) {
-        case "ping":
-          console.log("[signalClient] Received a ping from the signaling server")
-          break;
-      }
-    }
+    this.socket.onmessage = this.onMessage
   }
 
   close = () => {
     console.log("[signalClient] Closing the connection...")
     this.socket?.close()
+    this.userId = null
+    this.peers = []
+  }
+
+  onOpen = () => {
+    console.log("[signalClient] Connection established...")
+
+    // Send a join message
+    // this.send({ type: "presence", action: "join" })
+  }
+
+  onMessage = event => {
+    const msg = JSON.parse(event.data)
+    switch (msg.type) {
+      case "ping":
+        console.log("[signalClient] Received a ping", msg)
+        break
+      case "welcome":
+        console.log("[signalClient] Received a welcome message", msg)
+        this.onWelcome(msg)
+        break
+      case "presence":
+        console.log("[signalClient] Received a presence update", msg)
+        this.onPresence(msg)
+        break
+      default:
+        console.error("[signalClient] unknown message type", msg)
+    }
+  }
+
+  onWelcome = (msg) => {
+    // Probably should call an event handler passed down by a parent class
+    // That class should then create direct connections to the other peers
+    const { peers, userId } = msg
+    this.userId = userId
+    this.peers = peers
+  }
+
+  onPresence = (msg) => {
+    if (msg.action === "join") {
+      // open a direct connection...
+      const { id } = msg
+      this.peers = this.peers.concat(id)
+    } else if (msg.action === "leave") {
+      // close the connection...
+    }
+  }
+
+  send = (json) => {
+    this.socket.send(JSON.stringify(json))
   }
 
   sendPing = () => {
     console.log("[signalClient] Sending a ping to the signaling server...")
-    this.socket.send(JSON.stringify({ type: "ping" }))
+    this.send({ type: "ping" })
   }
 }
 
